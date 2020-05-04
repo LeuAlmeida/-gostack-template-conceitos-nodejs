c832bce4b265d6cf59744e6e98b0b79be61ea80d
Léu Almeida <leo@webid.net.br>
2020-05-04 13:48:41 -0300

    feat(app): implementing PUT method

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  return response.json(repositories)
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
@@ -20,26 +20,62 @@ app.post("/repositories", (request, response) => {
   // Techs array validator
   if (!Array.isArray(techs)) {
     return response.status(400).json({
-      error: "Techs must be an array"
-    })
+      error: "Techs must be an array",
+    });
   }
-  
+
   // URL validator
-  const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+  const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 
   if (!regexp.test(url)) {
-    return response.status(400).json({ error: 'Invalid URL'})
+    return response.status(400).json({ error: "Invalid URL" });
   }
 
-  const project = { id: uuid(), title, url, techs, likes: 0 }
+  const project = { id: uuid(), title, url, techs, likes: 0 };
 
-  repositories.unshift(project)
+  repositories.unshift(project);
 
-  return response.json(repositories)
+  return response.json(repositories);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+   // Techs array validator
+   if (!Array.isArray(techs)) {
+    return response.status(400).json({
+      error: "Techs must be an array",
+    });
+  }
+
+  // URL validator
+  const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
+  if (!regexp.test(url)) {
+    return response.status(400).json({ error: "Invalid URL" });
+  }
+
+  // Repository validator
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repositoryIndex < 0) {
+    return response
+      .status(400)
+      .json({ error: "RepositoryIndex does not found." });
+  }
+
+  const repository = {
+    id,
+    title: title || repositories[repositoryIndex].title,
+    url: url || repositories[repositoryIndex].url,
+    techs: techs || repositories[repositoryIndex].techs,
+    likes: repositories[repositoryIndex].likes,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
